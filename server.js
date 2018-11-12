a11ef804f6b3049830a906ef2589d95e6a92005a
zpei <zhengqing.pei@gmail.com>
2018-11-12 16:41:05 -0500

    enabled ssr using localhost mongodb through server

--- server.js
+++ server.js
@@ -7,19 +7,13 @@ import { template } from './template';
 const port = process.env.PORT || 3000;
 const proxy = express()
 
-proxy.use(bodyParser());
-proxy.get('/buy/:prod_name', function(req, res) {
-  const prod_name = req.params.prod_name
-
-  //for overviews only:
-  axios
-    .get(`http://localhost:3001/buy/${prod_name}/overview`)
-    .then(({data: {product : initialState, overviewsHtml}}) => {
-        const html = template(initialState, overviewsHtml);
-        res.status(200).send(html);
-      }
-    )
-    .catch(e => console.log('there is an error!', e));
+proxy.use(bodyParser.urlencoded({extended: true}));
+proxy.get('/buy/:productName', (req, res) => {
+  const productName = req.params.productName
+  axios.get(`http://localhost:3001/mongo/${productName}`).then(({data: {initialState, overviewsHtml}}) => {
+    const page = template(initialState, overviewsHtml);
+    res.status(200).send(page);
+  }).catch(e => console.log('there is an error for /mongo/productName', e));
 });
 
 proxy.listen(port, function() {
--- template.js
+++ template.js
@@ -8,17 +8,19 @@ export const template = function(initialState = {}, html = '') {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Upscale</title>
     <link rel="stylesheet" href="http://localhost:3001/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
   </head>
   <body>
+    <script>window.__initialState__ = ${JSON.stringify(initialState)}</script>
 
-  <script>window.__initialState__ = ${JSON.stringify(initialState)}</script>
+    <div id="app">
+      <div id="product-overviews-app" style="margin-left: 15vw">${html}</div>
+    </div>
 
-  <div id="app">
-    <div id="product-overviews-app">${html}</div>
-  </div>
-
-  <script src="http://localhost:3001/bundle.js"></script>
-  
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+    <script src="http://localhost:3001/bundle.js"></script>
   </body>
   </html>
   `
